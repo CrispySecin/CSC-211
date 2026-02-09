1c5c6d63bf38a5bfde0b5709a39b8a1154614413
CrispySecin <sellocrymatinketja@gmail.com>
2026-02-09 16:24:32 +0200

    Refactor shufflebiased for improved shuffling logic
    Refactor shufflebiased class to use Random for shuffling and simplify logic.

--- shufflebiased.java
+++ shufflebiased.java
@@ -1,38 +1,22 @@
 import java.util.Arrays;
-import java.util.List;
+import java.util.Random;
 
-public class shufflebiased {
+class shufflebiased {
     public static void main(String[] args) {
-
-        int numbers[]=new int[100];
-        for (int i=0;i<100;i++){
-            numbers[i]=i;
+        int shuffled[] = new int[10];
+        int i;
+        for (i = 0; i < 10; i++){
+        shuffled[i] = i;
         }
-
-        int shuffled[]= new int[100];
-
-        for (int i =0; i<100; i++){
-            int number = 1+(int)(Math.random()*100);
-            for (int j:shuffled){
-                if (j==number){
-                    break;
-                } else {
-                    shuffled[i]=number;
-                }
-            }
-        }
-        System.out.print(Arrays.toString(shuffled));
-
-
-        /*the second shuffle*/
+        for (i=0; i<10; i++){
+        Random random = new Random();
+        int randomIndex = random.nextInt(shuffled.length);
+        int randomElement = shuffled[randomIndex];
         
+        shuffled[i]=randomElement;
+        }
+        
+        System.out.print(Arrays.toString(shuffled));
     }
 }
 
-
-/*git config --global user.name "Your Name" # Set global username
-git config --global user.email "you@example.com" # Set global email
-git config --list # View all configurations
-git help # Show help for commands
-
-*/
