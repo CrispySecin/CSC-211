b88154c7b32d65ef09b117511c96c0afba954cc9
CrispySecin <sellocrymatinketja@gmail.com>
2026-02-09 15:55:24 +0200

    Add files via upload

---
+++ shuffle1.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+import java.util.List;
+
+class shuffle1 {
+    public static void main(String[] args) {
+
+        int numbers[]=new int[100];
+        for (int i=0;i<100;i++){
+            numbers[i]=i;
+        }
+
+        int shuffled[]= new int[100];
+
+        for (int i =0; i<100; i++){
+            int number = 1+(int)(Math.random()*100);
+            for (int j:shuffled){
+                if (j==number){
+                    break;
+                } else {
+                    shuffled[i]=number;
+                }
+            }
+        }
+        System.out.print(Arrays.toString(shuffled));
+
+
+        /*the second shuffle*/
+        
+    }
+}
+
+
+/*git config --global user.name "Your Name" # Set global username
+git config --global user.email "you@example.com" # Set global email
+git config --list # View all configurations
+git help # Show help for commands
+
+*/
